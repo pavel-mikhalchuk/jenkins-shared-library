c81c54dee61f032ebe4cee91fad8c10c8c5ec20b
Pavel Mikhalchuk <pavel.mikhalchuk@gmail.com>
2020-11-04 21:23:49 +0300

    ASVCN-1563 ASVCN-1570 ASVCN-1564 ASVCN-1566 code reuse for helm and java deployment pipelines

---
+++ src/com/mikhalchuk/DeploymentPipelineHelper.groovy
@@ -0,0 +1,182 @@
+package com.mikhalchuk
+
+class DeploymentPipelineHelper {
+
+    def pipeline
+
+    public DeploymentPipelineHelper(pipeline) {
+        this.pipeline = pipeline
+    }
+
+    def defineMoreContextBasedOnUserInput(ctx) {
+        ctx.namespace = "${pipeline.params.NAMESPACE}"
+        ctx.dockerImage = "${ctx.service}:${pipeline.params.IMAGE_TAG ?: 'latest'}"
+        ctx.jenkinsBuildNumber = "${pipeline.JOB_NAME}-${pipeline.BUILD_NUMBER}"
+        ctx.currentBranchName = "${pipeline.BRANCH_NAME}"
+
+        ctx.infraFolder = pipeline.sh(script: 'echo infra-$(date +"%d-%m-%Y_%H-%M-%S")', returnStdout: true).trim()
+        ctx.helmChartFolder = "kubernetes/helm-chart/${ctx.service}"
+        ctx.helmRelease = "${ctx.service}-${ctx.namespace}"
+
+        //// env-specific (dev VS prod)
+        ctx.kubeStateFolder = "${ctx.infraFolder}/kube-${ctx.env}/cluster-state/alutech-services/${ctx.namespace}/${ctx.service}/raw-manifests"
+        ////  ////  //// //// //// ////////  ////  //// //// //// ////
+    }
+
+    def notifySlack(ctx) {
+        pipeline.script {
+            pipeline.wrap([$class: 'BuildUser']) {
+                pipeline.slackSend channel: "java_services", color: "good", message: "*${pipeline.BUILD_USER}* накатывает ветку *${ctx.currentBranchName}* на *${ctx.service} ${ctx.namespace}*.\n${ctx.dockerImage}\nСохраняйте спокойствие 😌"
+            }
+        }
+    }
+
+    def checkoutInfraRepo(ctx) {
+        pipeline.sh "mkdir ${ctx.infraFolder}"
+
+        pipeline.dir("${ctx.infraFolder}") {
+            pipeline.git credentialsId: 'jenkins', url: 'http://bb.alutech-mc.com:8080/scm/as/infra.git'
+        }
+    }
+
+    def copyConfigToHelmChart(ctx) {
+        pipeline.sh "cp src/main/resources/application.${ctx.namespace}.properties ${ctx.helmChartFolder}/application.properties"
+    }
+
+    def writeHelmValuesYaml(ctx) {
+        pipeline.writeFile file: "${ctx.helmChartFolder}/values.yaml", text: helmValues(ctx)
+    }
+
+    def helmValues(ctx) {
+        def result = merge(defaultValues(ctx), ctx.helmValues)
+        if (result.host) {
+            result.host = resolveIngressHost(result.host, ctx)
+        }
+        Yaml.write(result, pipeline).trim()
+    }
+
+    def static defaultValues(ctx) {
+        [
+            replicaCount: 1,
+            gitBranch: ctx.currentBranchName,
+            image: [
+                repository: 'dockerhub-vip.alutech.local',
+                tag: ctx.dockerImage,
+                pullPolicy: 'IfNotPresent'
+            ],
+            service: [
+                externalPort: 80,
+                internalPort: 8080
+            ],
+            jenkinsBuildNumber: ctx.jenkinsBuildNumber,
+            environment: ctx.env == 'prod' ? "${ctx.env}" : ""
+        ]
+    }
+
+    def merge(Map lhs, Map rhs) {
+        return rhs.inject(lhs.clone()) { map, entry ->
+            if (map[entry.key] instanceof Map && entry.value instanceof Map) {
+                map[entry.key] = merge(map[entry.key], entry.value)
+            } else if (map[entry.key] instanceof Collection && entry.value instanceof Collection) {
+                map[entry.key] += entry.value
+            } else {
+                map[entry.key] = entry.value
+            }
+            return map
+        }
+    }
+
+    def generateK8SManifests(ctx) {
+        pipeline.sh "rm -rf ${ctx.kubeStateFolder}"
+        pipeline.sh "mkdir -p ${ctx.kubeStateFolder}"
+
+        pipeline.sh "helm template --namespace ${ctx.namespace} --name ${ctx.helmRelease} ${ctx.helmChartFolder} > '${ctx.kubeStateFolder}/kube-state.yaml'"
+    }
+
+    def pushK8SManifests(ctx) {
+        pipeline.dir("${ctx.infraFolder}") {
+            pipeline.withCredentials([usernamePassword(credentialsId: 'jenkins', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
+                pipeline.sh 'git config user.email "jenkins@infra.tower"'
+                pipeline.sh 'git config user.name "Jenkins"'
+
+                pipeline.sh 'git add *'
+                pipeline.sh 'git commit -m "[jenkins]: ${JOB_NAME} - ${BUILD_NUMBER}"'
+                pipeline.sh 'git push http://${GIT_USERNAME}:${GIT_PASSWORD}@bb.alutech-mc.com:8080/scm/as/infra.git HEAD:master'
+            }
+        }
+    }
+
+    def notifyArgoCD() {
+        pipeline.sh 'curl -k -X POST https://git-events-publisher.in.in.alutech24.com/push'
+    }
+
+    def static resolveIngressHost(host, ctx) {
+        if (host instanceof Closure) {
+            def hostByNs = { ns->
+                return ns.contains('-') ? "${ns.split('-')[1]}.${ns.split('-')[0]}" : ns
+            }
+
+            def ingUtils = [
+                svc_ns_inin: {
+                    return "${ctx.service}.${hostByNs(ctx.namespace)}.in.in.alutech24.com"
+                },
+                svc_prod: {
+                    return "${ctx.service}.alutech24.com"
+                }
+            ]
+            return host.call(ingUtils)
+        } else {
+            return host
+        }
+    }
+
+    def initDockerImageChoiceParameter(ctx) {
+        pipeline.properties([
+            pipeline.parameters([
+                [
+                    $class: 'ChoiceParameter',
+                    choiceType: 'PT_SINGLE_SELECT',
+                    description: 'Docker image tags',
+                    filterLength: 1,
+                    filterable: true,
+                    name: 'IMAGE_TAG',
+                    script: [
+                        $class: 'GroovyScript',
+                        fallbackScript: [classpath: [], sandbox: true, script: '["error :("]'],
+                        script: [classpath: [], sandbox: true, script: """
+                        import java.util.logging.Level 
+                        import java.util.logging.Logger
+
+                        import groovy.json.JsonSlurper
+
+                        def log = Logger.getLogger("com.alutech.activechoice.dockerhub")
+
+                        def parse = { response ->
+                            log.info("Parsing response...")
+
+                            def tags = response?.trim() ? new JsonSlurper().parseText(response).tags : []
+
+                            log.info("Parsed!")
+
+                            def hasLatest = tags.remove("latest")
+                            return hasLatest ? tags.plus(0, ["latest"]) : tags
+                        }
+
+                        try {      
+                            log.info("Start fetching tags...")
+
+                            def response = ["curl", "-k", "https://dockerhub-vip.alutech.local/v2/${ctx.service}/tags/list"].execute().text
+
+                            log.info("Response from docker hub: " + response)
+
+                            return parse(response)
+                        } catch (Exception e) {
+                            e.printStackTrace()
+                        }
+                    """]
+                    ]
+                ]
+            ])
+        ])
+    }
+}
--- vars/javaMicroserviceDeploymentPipeline.groovy
+++ vars/javaMicroserviceDeploymentPipeline.groovy
@@ -3,7 +3,9 @@ import com.mikhalchuk.*
 def call(body) {
     def ctx = setUpContext(body)
 
-    initDockerImageChoiceParameter(ctx)
+    def helper = new DeploymentPipelineHelper(this)
+
+    helper.initDockerImageChoiceParameter(ctx)
 
     pipeline {
         agent { label 'helm-deploy' }
@@ -21,33 +23,33 @@ def call(body) {
                     // This step is very important!!! 
                     // Please do not remove it unless you find a better way without introducing "Init" stage because it's ugly :)"
                     // Later stages depend on it.
-                    defineMoreContextBasedOnUserInput(ctx)
+                    helper.defineMoreContextBasedOnUserInput(ctx)
 
-                    notifySlack(ctx)
+                    helper.notifySlack(ctx)
                 }
             }
             stage('checkout infra repo') {
                 steps {
-                    checkoutInfraRepo(ctx)
+                    helper.checkoutInfraRepo(ctx)
                 }
             }
             stage('generate K8S manifests') {
                 steps {
                     container('helm') {
-                        copyConfigToHelmChart(ctx)
-                        writeHelmValuesYaml(ctx)
-                        generateK8SManifests(ctx)
+                        helper.copyConfigToHelmChart(ctx)
+                        helper.writeHelmValuesYaml(ctx)
+                        helper.generateK8SManifests(ctx)
                     }
                 }
             }
             stage('push K8S manifests to infra repo') {
                 steps {
-                    pushK8SManifests(ctx)
+                    helper.pushK8SManifests(ctx)
                 }
             }
             stage('notify ArgoCD') {
                 steps {
-                    notifyArgoCD()
+                    helper.notifyArgoCD()
                 }
             }
         }
@@ -60,176 +62,4 @@ def setUpContext(body) {
 
     // defining more parameters for ourselves
     return ctx
-}
-
-def defineMoreContextBasedOnUserInput(ctx) {
-    ctx.namespace = "${params.NAMESPACE}"
-    ctx.dockerImage = "${ctx.service}:${params.IMAGE_TAG ?: 'latest'}"
-    ctx.jenkinsBuildNumber = "${JOB_NAME}-${BUILD_NUMBER}"
-    ctx.currentBranchName = "${BRANCH_NAME}"
-
-    ctx.infraFolder = sh(script: 'echo infra-$(date +"%d-%m-%Y_%H-%M-%S")', returnStdout: true).trim()
-    ctx.helmChartFolder = "kubernetes/helm-chart/${ctx.service}"
-    ctx.helmRelease = "${ctx.service}-${ctx.namespace}"
-    
-    //// env-specific (dev VS prod)
-    ctx.kubeStateFolder = "${ctx.infraFolder}/kube-${ctx.env}/cluster-state/alutech-services/${ctx.namespace}/${ctx.service}/raw-manifests"
-    ////  ////  //// //// //// ////////  ////  //// //// //// ////
-}
-
-def notifySlack(ctx) {
-    script {
-        wrap([$class: 'BuildUser']) {
-            slackSend channel: "java_services", color: "good", message: "*${BUILD_USER}* накатывает ветку *${ctx.currentBranchName}* на *${ctx.service} ${ctx.namespace}*.\n${ctx.dockerImage}\nСохраняйте спокойствие 😌"
-        }
-    }
-}
-
-def checkoutInfraRepo(ctx) {
-    sh "mkdir ${ctx.infraFolder}"
-
-    dir("${ctx.infraFolder}") {
-        git credentialsId: 'jenkins', url: 'http://bb.alutech-mc.com:8080/scm/as/infra.git'
-    }
-}
-
-def copyConfigToHelmChart(ctx) {
-    sh "cp src/main/resources/application.${ctx.namespace}.properties ${ctx.helmChartFolder}/application.properties"
-}
-
-def writeHelmValuesYaml(ctx) {
-    writeFile file: "${ctx.helmChartFolder}/values.yaml", text: helmValues(ctx)
-}
-
-def helmValues(ctx) {
-    def result = merge(defaultValues(ctx), ctx.helmValues)
-    if (result.host) {
-        result.host = resolveIngressHost(result.host, ctx)
-    }
-    Yaml.write(result).trim()
-}
-
-def static defaultValues(ctx) {
-    [
-        replicaCount: 1,
-        gitBranch: ctx.currentBranchName,
-        image: [
-            repository: 'dockerhub-vip.alutech.local',
-            tag: ctx.dockerImage,
-            pullPolicy: 'IfNotPresent'
-        ],
-        service: [
-            externalPort: 80,
-            internalPort: 8080
-        ],
-        jenkinsBuildNumber: ctx.jenkinsBuildNumber,
-        environment: ctx.env == 'prod' ? "${ctx.env}" : ""
-    ]
-}
-
-def merge(Map lhs, Map rhs) {
-    return rhs.inject(lhs.clone()) { map, entry ->
-        if (map[entry.key] instanceof Map && entry.value instanceof Map) {
-            map[entry.key] = merge(map[entry.key], entry.value)
-        } else if (map[entry.key] instanceof Collection && entry.value instanceof Collection) {
-            map[entry.key] += entry.value
-        } else {
-            map[entry.key] = entry.value
-        }
-        return map
-    }
-}
-
-def generateK8SManifests(ctx) {
-    sh "rm -rf ${ctx.kubeStateFolder}"
-    sh "mkdir -p ${ctx.kubeStateFolder}"
-
-    sh "helm template --namespace ${ctx.namespace} --name ${ctx.helmRelease} ${ctx.helmChartFolder} > '${ctx.kubeStateFolder}/kube-state.yaml'"
-}
-
-def pushK8SManifests(ctx) {
-    dir("${ctx.infraFolder}") {
-        withCredentials([usernamePassword(credentialsId: 'jenkins', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
-            sh 'git config user.email "jenkins@infra.tower"'
-            sh 'git config user.name "Jenkins"'
-
-            sh 'git add *'
-            sh 'git commit -m "[jenkins]: ${JOB_NAME} - ${BUILD_NUMBER}"'
-            sh 'git push http://${GIT_USERNAME}:${GIT_PASSWORD}@bb.alutech-mc.com:8080/scm/as/infra.git HEAD:master'
-        }                        
-    }
-}
-
-def notifyArgoCD() {
-    sh 'curl -k -X POST https://git-events-publisher.in.in.alutech24.com/push'
-}
-
-def static resolveIngressHost(host, ctx) {
-    if (host instanceof Closure) {
-        def hostByNs = { ns->
-            return ns.contains('-') ? "${ns.split('-')[1]}.${ns.split('-')[0]}" : ns
-        }
-
-        def ingUtils = [
-            svc_ns_inin: {
-                return "${ctx.service}.${hostByNs(ctx.namespace)}.in.in.alutech24.com"
-            },
-            svc_prod: {
-                return "${ctx.service}.alutech24.com"
-            }
-        ]
-        return host.call(ingUtils)
-    } else {
-        return host
-    }
-}
-
-def initDockerImageChoiceParameter(ctx) {
-    properties([
-        parameters([
-            [
-                $class: 'ChoiceParameter', 
-                choiceType: 'PT_SINGLE_SELECT', 
-                description: 'Docker image tags', 
-                filterLength: 1,
-                filterable: true,
-                name: 'IMAGE_TAG', 
-                script: [
-                    $class: 'GroovyScript', 
-                    fallbackScript: [classpath: [], sandbox: true, script: '["error :("]'], 
-                    script: [classpath: [], sandbox: true, script: """
-                        import java.util.logging.Level 
-                        import java.util.logging.Logger
-
-                        import groovy.json.JsonSlurper
-
-                        def log = Logger.getLogger("com.alutech.activechoice.dockerhub")
-
-                        def parse = { response ->
-                            log.info("Parsing response...")
-
-                            def tags = response?.trim() ? new JsonSlurper().parseText(response).tags : []
-                            
-                            log.info("Parsed!")
-                            
-                            def hasLatest = tags.remove("latest")
-                            return hasLatest ? tags.plus(0, ["latest"]) : tags
-                        }
-
-                        try {      
-                            log.info("Start fetching tags...")
-                            
-                            def response = ["curl", "-k", "https://dockerhub-vip.alutech.local/v2/${ctx.service}/tags/list"].execute().text
-                            
-                            log.info("Response from docker hub: " + response)
-
-                            return parse(response)
-                        } catch (Exception e) {
-                            e.printStackTrace()
-                        }
-                    """]
-                ]
-            ]
-        ])
-    ])
 }
