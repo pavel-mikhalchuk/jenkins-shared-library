e652450e4e97b5ea29d5b3fcd2a55b98c0b8b207
Pavel Mikhalchuk <pavel.mikhalchuk@gmail.com>
2021-09-20 14:40:46 +0300

    debugging dockerBuild step in helper

--- src/com/mikhalchuk/DeploymentPipelineHelper.groovy
+++ src/com/mikhalchuk/DeploymentPipelineHelper.groovy
@@ -27,7 +27,7 @@ class DeploymentPipelineHelper {
         pipeline.echo "Before build..."
         pipeline.echo "${ctx.toString()}"
         ctx.containerImages.each {
-            echo "In build - ${it}"
+            pipeline.echo "In build - ${it}"
 
             def img = (it.name + ':' + dockerImgTag(ctx)).toLowerCase()
             def imgLatest = (it.name + ':latest').toLowerCase()
@@ -38,7 +38,7 @@ class DeploymentPipelineHelper {
             ctx.dockerImages << img
             ctx.dockerImages << imgLatest
         }
-        echo "After build..."
+        pipeline.echo "After build..."
     }
 
     def dockerImgTag(ctx) {
